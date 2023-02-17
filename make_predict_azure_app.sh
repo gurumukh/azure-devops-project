4a292e0593d52b409fdc15dd5b7ba94d87da39e6
gurmukh singh <gurmukh.singh@bertelsmann.de>
2023-02-17 10:00:11 +0800

    change ml model

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-pro.azurewebsites.net:$PORT/predict 
+     -X POST localhost:$PORT/predict 
      #your application name <yourappname>goes here
